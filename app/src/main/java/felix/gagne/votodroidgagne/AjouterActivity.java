a9d770f008a8d394a9c8d27636e26de8bef1d8d8
Félix Gagné <2070094@cegepmontpetit.ca>
2021-10-07 18:22:23 -0400

    Finalisation de la page d'ajout.
    Finalisation du layout de la page d'ajout.

--- app/src/main/java/felix/gagne/votodroidgagne/AjouterActivity.java
+++ app/src/main/java/felix/gagne/votodroidgagne/AjouterActivity.java
@@ -2,14 +2,35 @@ package felix.gagne.votodroidgagne;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import felix.gagne.votodroidgagne.databinding.ActivityAjouterBinding;
+import felix.gagne.votodroidgagne.databinding.ActivityMainBinding;
 
 public class AjouterActivity extends AppCompatActivity {
+
+    ActivityAjouterBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTitle("VoteDroid");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajouter);
+
+
+        binding = ActivityAjouterBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.poseLaQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i1 = new Intent(AjouterActivity.this, MainActivity.class);
+                startActivity(i1);
+            }
+        });
     }
 }
