2b2069ee71cc2810f0db9c04fac8d1716f79f3dc
Félix <2070094@cegepmontpetit.ca>
2021-10-27 14:40:40 -0400

    Re: Création du graphique
    J'ai oublié de stage les files donc ceci est mon graphique. (pour de vrai)

--- app/src/main/java/felix/gagne/votodroidgagne/ResultatActivity.java
+++ app/src/main/java/felix/gagne/votodroidgagne/ResultatActivity.java
@@ -3,24 +3,95 @@ package felix.gagne.votodroidgagne;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-import felix.gagne.votodroidgagne.databinding.ActivityMainBinding;
-import felix.gagne.votodroidgagne.databinding.ActivityResultatBinding;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.DefaultAxisValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ResultatActivity extends AppCompatActivity {
 
-    ActivityResultatBinding binding;
+    BarChart chart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        // initialiser l'ecran
-        binding = ActivityResultatBinding.inflate(getLayoutInflater());
-        View view = binding.getRoot();
-        setContentView(view);
-
+        setContentView(R.layout.activity_resultat);
         setTitle("VotoDroid");
+
+        chart = findViewById(R.id.graph);
+
+
+        /* Settings for the graph - Change me if you want*/
+        chart.setMaxVisibleValueCount(6);
+        chart.setPinchZoom(false);
+        chart.setDrawGridBackground(false);
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        xAxis.setGranularity(1); // only intervals of 1 day
+        xAxis.setLabelCount(6);
+        xAxis.setValueFormatter(new DefaultAxisValueFormatter(0));
+
+        YAxis leftAxis = chart.getAxisLeft();
+        leftAxis.setLabelCount(5, false);
+        leftAxis.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART);
+        leftAxis.setSpaceTop(15f);
+        leftAxis.setGranularity(1);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setValueFormatter(new DefaultAxisValueFormatter(0));
+        chart.getDescription().setEnabled(false);
+        chart.getAxisRight().setEnabled(false);
+
+        /* Data and function call to bind the data to the graph */
+        Map<Integer, Integer> dataGraph = new HashMap<Integer, Integer>() {{
+            put(1, 4);
+            put(2, 3);
+            put(3, 5);
+            put(4, 2);
+            put(5, 1);
+        }};
+        setData(dataGraph);
+    }
+
+    private void setData(Map<Integer, Integer> datas) {
+
+        ArrayList<BarEntry> values = new ArrayList<>();
+
+        /* Every bar entry is a bar in the graphic */
+        for (Map.Entry<Integer, Integer> key : datas.entrySet()){
+            values.add(new BarEntry(key.getKey() , key.getValue()));
+        }
+
+        BarDataSet set1;
+        if (chart.getData() != null &&
+                chart.getData().getDataSetCount() > 0) {
+            set1 = (BarDataSet) chart.getData().getDataSetByIndex(0);
+            set1.setValues(values);
+            chart.getData().notifyDataChanged();
+            chart.notifyDataSetChanged();
+        } else {
+            set1 = new BarDataSet(values, "Points");
+
+            set1.setDrawIcons(false);
+
+            ArrayList<IBarDataSet> dataSets = new ArrayList<>();
+            dataSets.add(set1);
+
+            BarData data = new BarData(dataSets);
+            data.setValueTextSize(10f);
+            data.setBarWidth(.9f);
+            chart.setData(data);
+        }
     }
 }
